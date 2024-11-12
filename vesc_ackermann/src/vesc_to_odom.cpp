a0365d480971c1bf1f7b969d34316b18779316c7
JHA PUSHPANJALI <141298200+Jha1005@users.noreply.github.com>
2024-11-12 18:25:19 +0530

    Update vesc_to_odom.cpp

--- vesc_ackermann/src/vesc_to_odom.cpp
+++ vesc_ackermann/src/vesc_to_odom.cpp
@@ -30,57 +30,62 @@
 #include <cmath>
 #include <string>
 
-#include <nav_msgs/Odometry.h>
-#include <geometry_msgs/TransformStamped.h>
+#include <nav_msgs/msg/odometry.hpp>
+#include <geometry_msgs/msg/transform_stamped.hpp>
+#include <tf2_ros/transform_broadcaster.h>
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/float64.hpp>
 
 namespace vesc_ackermann
 {
 
 template <typename T>
-inline bool getRequiredParam(const ros::NodeHandle& nh, const std::string& name, T* value);
+bool getRequiredParam(const std::shared_ptr<rclcpp::Node>& node, const std::string& name, T* value);
 
-VescToOdom::VescToOdom(ros::NodeHandle nh, ros::NodeHandle private_nh) :
-  odom_frame_("odom"), base_frame_("base_link"),
+VescToOdom::VescToOdom(std::shared_ptr<rclcpp::Node> node) :
+  node_(node), odom_frame_("odom"), base_frame_("base_link"),
   use_servo_cmd_(true), publish_tf_(false), x_(0.0), y_(0.0), yaw_(0.0)
 {
   // get ROS parameters
-  private_nh.param("odom_frame", odom_frame_, odom_frame_);
-  private_nh.param("base_frame", base_frame_, base_frame_);
-  private_nh.param("use_servo_cmd_to_calc_angular_velocity", use_servo_cmd_, use_servo_cmd_);
-  if (!getRequiredParam(nh, "speed_to_erpm_gain", &speed_to_erpm_gain_))
+  node_->declare_parameter("odom_frame", odom_frame_);
+  node_->declare_parameter("base_frame", base_frame_);
+  node_->declare_parameter("use_servo_cmd_to_calc_angular_velocity", use_servo_cmd_);
+  node_->declare_parameter("publish_tf", publish_tf_);
+  
+  if (!getRequiredParam(node_, "speed_to_erpm_gain", &speed_to_erpm_gain_))
     return;
-  if (!getRequiredParam(nh, "speed_to_erpm_offset", &speed_to_erpm_offset_))
+  if (!getRequiredParam(node_, "speed_to_erpm_offset", &speed_to_erpm_offset_))
     return;
   if (use_servo_cmd_)
   {
-    if (!getRequiredParam(nh, "steering_angle_to_servo_gain", &steering_to_servo_gain_))
+    if (!getRequiredParam(node_, "steering_angle_to_servo_gain", &steering_to_servo_gain_))
       return;
-    if (!getRequiredParam(nh, "steering_angle_to_servo_offset", &steering_to_servo_offset_))
+    if (!getRequiredParam(node_, "steering_angle_to_servo_offset", &steering_to_servo_offset_))
       return;
-    if (!getRequiredParam(nh, "wheelbase", &wheelbase_))
+    if (!getRequiredParam(node_, "wheelbase", &wheelbase_))
       return;
   }
-  private_nh.param("publish_tf", publish_tf_, publish_tf_);
 
   // create odom publisher
-  odom_pub_ = nh.advertise<nav_msgs::Odometry>("odom", 10);
+  odom_pub_ = node_->create_publisher<nav_msgs::msg::Odometry>("odom", 10);
 
   // create tf broadcaster
   if (publish_tf_)
   {
-    tf_pub_.reset(new tf::TransformBroadcaster);
+    tf_broadcaster_ = std::make_shared<tf2_ros::TransformBroadcaster>(node_);
   }
 
-  // subscribe to vesc state and. optionally, servo command
-  vesc_state_sub_ = nh.subscribe("sensors/core", 10, &VescToOdom::vescStateCallback, this);
+  // subscribe to vesc state and optionally, servo command
+  vesc_state_sub_ = node_->create_subscription<vesc_msgs::msg::VescStateStamped>(
+      "sensors/core", 10, std::bind(&VescToOdom::vescStateCallback, this, std::placeholders::_1));
   if (use_servo_cmd_)
   {
-    servo_sub_ = nh.subscribe("sensors/servo_position_command", 10,
-                              &VescToOdom::servoCmdCallback, this);
+    servo_sub_ = node_->create_subscription<std_msgs::msg::Float64>(
+        "sensors/servo_position_command", 10, std::bind(&VescToOdom::servoCmdCallback, this, std::placeholders::_1));
   }
 }
 
-void VescToOdom::vescStateCallback(const vesc_msgs::VescStateStamped::ConstPtr& state)
+void VescToOdom::vescStateCallback(const vesc_msgs::msg::VescStateStamped::SharedPtr state)
 {
   // check that we have a last servo command if we are depending on it for angular velocity
   if (use_servo_cmd_ && !last_servo_cmd_)
@@ -105,23 +110,21 @@ void VescToOdom::vescStateCallback(const vesc_msgs::VescStateStamped::ConstPtr& 
     last_state_ = state;
 
   // calc elapsed time
-  ros::Duration dt = state->header.stamp - last_state_->header.stamp;
+  rclcpp::Duration dt = state->header.stamp - last_state_->header.stamp;
 
-  /** @todo could probably do better propigating odometry, e.g. trapezoidal integration */
-
-  // propigate odometry
+  // propagate odometry
   double x_dot = current_speed * cos(yaw_);
   double y_dot = current_speed * sin(yaw_);
-  x_ += x_dot * dt.toSec();
-  y_ += y_dot * dt.toSec();
+  x_ += x_dot * dt.seconds();
+  y_ += y_dot * dt.seconds();
   if (use_servo_cmd_)
-    yaw_ += current_angular_velocity * dt.toSec();
+    yaw_ += current_angular_velocity * dt.seconds();
 
   // save state for next time
   last_state_ = state;
 
   // publish odometry message
-  nav_msgs::Odometry::Ptr odom(new nav_msgs::Odometry);
+  auto odom = std::make_shared<nav_msgs::msg::Odometry>();
   odom->header.frame_id = odom_frame_;
   odom->header.stamp = state->header.stamp;
   odom->child_frame_id = base_frame_;
@@ -129,59 +132,45 @@ void VescToOdom::vescStateCallback(const vesc_msgs::VescStateStamped::ConstPtr& 
   // Position
   odom->pose.pose.position.x = x_;
   odom->pose.pose.position.y = y_;
-  odom->pose.pose.orientation.x = 0.0;
-  odom->pose.pose.orientation.y = 0.0;
   odom->pose.pose.orientation.z = sin(yaw_ / 2.0);
   odom->pose.pose.orientation.w = cos(yaw_ / 2.0);
 
   // Position uncertainty
-  /** @todo Think about position uncertainty, perhaps get from parameters? */
   odom->pose.covariance[0]  = 0.2;  ///< x
   odom->pose.covariance[7]  = 0.2;  ///< y
   odom->pose.covariance[35] = 0.4;  ///< yaw
 
-  // Velocity ("in the coordinate frame given by the child_frame_id")
+  // Velocity
   odom->twist.twist.linear.x = current_speed;
-  odom->twist.twist.linear.y = 0.0;
   odom->twist.twist.angular.z = current_angular_velocity;
-
-  // Velocity uncertainty
-  /** @todo Think about velocity uncertainty */
 
   if (publish_tf_)
   {
-    geometry_msgs::TransformStamped tf;
+    geometry_msgs::msg::TransformStamped tf;
     tf.header.frame_id = odom_frame_;
     tf.child_frame_id = base_frame_;
-    tf.header.stamp = ros::Time::now();
+    tf.header.stamp = node_->get_clock()->now();
     tf.transform.translation.x = x_;
     tf.transform.translation.y = y_;
-    tf.transform.translation.z = 0.0;
     tf.transform.rotation = odom->pose.pose.orientation;
-    if (ros::ok())
-    {
-      tf_pub_->sendTransform(tf);
-    }
+    tf_broadcaster_->sendTransform(tf);
   }
 
-  if (ros::ok())
-  {
-    odom_pub_.publish(odom);
-  }
+  odom_pub_->publish(*odom);
 }
 
-void VescToOdom::servoCmdCallback(const std_msgs::Float64::ConstPtr& servo)
+void VescToOdom::servoCmdCallback(const std_msgs::msg::Float64::SharedPtr servo)
 {
   last_servo_cmd_ = servo;
 }
 
 template <typename T>
-inline bool getRequiredParam(const ros::NodeHandle& nh, const std::string& name, T* value)
+bool getRequiredParam(const std::shared_ptr<rclcpp::Node>& node, const std::string& name, T* value)
 {
-  if (nh.getParam(name, *value))
+  if (node->get_parameter(name, *value))
     return true;
 
-  ROS_FATAL("VescToOdom: Parameter %s is required.", name.c_str());
+  RCLCPP_FATAL(node->get_logger(), "VescToOdom: Parameter %s is required.", name.c_str());
   return false;
 }
 
